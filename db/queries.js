f6a76d2e7a9bb0e14c835314b26c793638aaffd3
whimsy <lofu6@yahoo.com>
2025-08-08 04:09:55 -0400

    first commit pre pg

--- db/queries.js
+++ db/queries.js
@@ -1,15 +1,23 @@
-const pool = require('./pool');
+const pool = require("./pool");
 
 async function getAllUsernames() {
-  const { rows } = await pool.query('SELECT * FROM usernames');
+  const { rows } = await pool.query("SELECT * FROM usernames");
   return rows;
 }
 
 async function insertUsername(username) {
-  await pool.query('INSERT INTO usernames (username) VALUES ($1)', [username]);
+  await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
 }
 
+async function searchUsernames(searchTerm) {
+  const { rows } = await pool.query(
+    "SELECT * FROM usernames WHERE username LIKE $1",
+    [`%${searchTerm}%`]
+  );
+  return rows;
+}
 module.exports = {
   getAllUsernames,
-  insertUsername
+  insertUsername,
+  searchUsernames,
 };
